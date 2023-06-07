3b24090e7e79cf23e2d4b32725d6206431d9044f
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-06-07 14:13:25 -0600

    added customer import command for testing

--- app/Console/Commands/ImportCustomers.php
+++ app/Console/Commands/ImportCustomers.php
@@ -19,40 +19,57 @@ class ImportCustomers extends Command
     public function handle()
     {
         // Retrieve data from the old LbCustomer table
-        $oldCustomers = DB::connection('old')->table('LbCustomer')->get();
+        $oldCustomers = DB::connection('mysql-old')->table('LbCustomer')->get();
+
+        $totalCustomers = count($oldCustomers);
+        $processedCustomers = 0;
+
+        // Start the progress bar
+        $progressBar = $this->output->createProgressBar($totalCustomers);
+        $progressBar->start();
 
         // Import each customer to the new customers table
         foreach ($oldCustomers as $oldCustomer) {
-            DB::table('customers')->insert([
-                'name' => $oldCustomer->name,
-                'description' => $oldCustomer->description,
-                'rates' => $oldCustomer->rates,
-                'email' => $oldCustomer->email,
-                'technical_contact_email' => $oldCustomer->technicalContactEmail,
-                'phone' => $oldCustomer->phone,
-                'license' => $oldCustomer->license,
-                'address' => $oldCustomer->address,
-                'city' => $oldCustomer->city,
-                'state' => $oldCustomer->state,
-                'zip' => $oldCustomer->zip,
-                'website' => $oldCustomer->website,
-                'template' => $oldCustomer->template,
-                'level' => $oldCustomer->level,
-                'settings' => $oldCustomer->settings,
-                'disclosure' => $oldCustomer->disclosure,
-                'estimated_mortgage_disclosure' => $oldCustomer->estimatedMortgageDisclosure,
-                'hubspot' => $oldCustomer->hubspotId,
-                'block_mre' => $oldCustomer->blockMre,
-                'block_login_as' => $oldCustomer->blockLoginAs,
-                'ep' => $oldCustomer->ep,
-                'created_at' => $oldCustomer->createdAt,
-                'updated_at' => $oldCustomer->updatedAt,
-                'client_id' => $oldCustomer->clientId,
-                'active' => $oldCustomer->active,
-                'published' => 0, // Set the default value for the published column
-            ]);
+            DB::table('customers')->updateOrInsert(
+                ['name' => $oldCustomer->name],
+                [
+                    'description' => $oldCustomer->description,
+                    'rates' => $oldCustomer->rates,
+                    'email' => $oldCustomer->email,
+                    'technical_contact_email' => $oldCustomer->technicalContactEmail,
+                    'phone' => $oldCustomer->phone,
+                    'license' => $oldCustomer->license,
+                    'address' => $oldCustomer->address,
+                    'city' => $oldCustomer->city,
+                    'state' => $oldCustomer->state,
+                    'zip' => $oldCustomer->zip,
+                    'website' => $oldCustomer->website,
+                    'template' => $oldCustomer->template,
+                    'level' => $oldCustomer->level,
+                    //'settings' => $oldCustomer->settings,
+                    'disclosure' => $oldCustomer->disclosure,
+                    'estimated_mortgage_disclosure' => $oldCustomer->estimatedMortgageDisclosure,
+                    'hubspot' => $oldCustomer->hubspotId,
+                    'block_mre' => $oldCustomer->blockMre,
+                    'block_login_as' => $oldCustomer->blockLoginAs,
+                    'ep' => $oldCustomer->ep,
+                    'created_at' => $oldCustomer->createdAt,
+                    'updated_at' => $oldCustomer->updatedAt,
+                    // 'client_id' => $oldCustomer->clientId,
+                    'active' => $oldCustomer->active,
+                    'published' => $oldCustomer->active, // Set the default value for the published column
+                ]
+            );
+
+            // Update the progress bar and increment the processed customers count
+            $progressBar->advance();
+            $processedCustomers++;
         }
 
-        $this->info('Customers imported successfully.');
+        // Finish the progress bar
+        $progressBar->finish();
+
+        // Output the summary
+        $this->info("\nCustomers imported successfully. Total customers: $totalCustomers");
     }
 }
