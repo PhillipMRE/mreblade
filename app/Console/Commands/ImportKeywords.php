f1fff2d268ff1b14e090a0b7f08fd96351544d5b
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-06-08 13:46:28 -0600

    added telescope

--- app/Console/Commands/ImportKeywords.php
+++ app/Console/Commands/ImportKeywords.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use App\Models\Keyword;
-use App\Models\Customer;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Command;
 use Carbon\Carbon;
@@ -15,61 +14,92 @@ class ImportKeywords extends Command
 
     public function handle()
     {
-
+        DB::connection()->disableQueryLog();
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        $chunkSize = 500; 
+
+        $chunkSize = 500;
         $totalKeywords = DB::connection('mysql-old')->table('LbKeyword')->count();
         $bar = $this->output->createProgressBar($totalKeywords);
         $bar->setFormat("Processing... %current%/%max% %bar% %percent:3s%%\nEstimated Time: %estimated%\nElapsed Time: %elapsed%");
         $bar->start();
 
         $startTime = Carbon::now();
-        $elapsedTime = null;
+        $chunksCount = 0;
 
-        DB::connection('mysql-old')->table('LbKeyword')->orderBy('id')->chunk($chunkSize, function ($oldKeywords) use ($bar, $totalKeywords, $startTime, &$elapsedTime, $chunkSize) {
-            $processedKeywords = 0;
+        DB::connection('mysql-old')->table('LbKeyword')->orderBy('id')->chunk($chunkSize, function ($oldKeywords) use ($bar, $totalKeywords, $startTime, &$chunksCount, $chunkSize) {
+            $chunksCount++;
+            $keywordData = [];
+
             foreach ($oldKeywords as $oldKeyword) {
-                $keywordData = [
-                    'name' => $oldKeyword->name,
-                    'template' => $oldKeyword->template,
-                    'map' => $oldKeyword->map,
-                    'house_types' => $oldKeyword->houseTypes,
-                    'status_types' => $oldKeyword->statusTypes,
-                    'shortcode' => $oldKeyword->shortcode,
-                    'listing_settings' => $oldKeyword->listingSettings,
-                    'sponsor_only' => $oldKeyword->sponsorOnly,
-                    'show_solds' => $oldKeyword->showSolds,
-                    'use_version_5' => $oldKeyword->useVersion5,
-                    'active' => $oldKeyword->active,
-                    'listhub' => $oldKeyword->listhub,
-                    'created_at' => $oldKeyword->createdAt,
-                    'updated_at' => $oldKeyword->updatedAt,
-                    'customer_id' => $oldKeyword->customerId,
-                ];
+                    $keywordData[] = [
+                        'name' => $oldKeyword->name,
+                        'template' => $oldKeyword->template,
+                        'map' => $oldKeyword->map,
+                        'house_types' => $oldKeyword->houseTypes,
+                        'status_types' => $oldKeyword->statusTypes,
+                        'shortcode' => $oldKeyword->shortcode,
+                        'listing_settings' => $oldKeyword->listingSettings,
+                        'sponsor_only' => $oldKeyword->sponsorOnly,
+                        'show_solds' => $oldKeyword->showSolds,
+                        'use_version_5' => $oldKeyword->useVersion5,
+                        'active' => $oldKeyword->active,
+                        'listhub' => $oldKeyword->listhub,
+                        'created_at' => $oldKeyword->createdAt,
+                        'updated_at' => $oldKeyword->updatedAt,
+                        'customer_id' => $oldKeyword->customerId,
+                    ];
 
-                Keyword::updateOrCreate(
-                    ['name' => $oldKeyword->name],
-                    $keywordData
-                );
-
-                $processedKeywords++;
                 $bar->advance();
-
-                $currentTime = Carbon::now();
-                $elapsedTime = $currentTime->diffInSeconds($startTime);
-                $averageTimePerKeyword = $elapsedTime / $processedKeywords;
-                $remainingKeywords = max(0, $totalKeywords - ($processedKeywords * $chunkSize));
-                $estimatedTimeInSeconds = $averageTimePerKeyword * $remainingKeywords;
-                $estimatedTime = Carbon::now()->addSeconds($estimatedTimeInSeconds);
-                $bar->setMessage($estimatedTime->diffForHumans(null, true, false), 'estimated');
-                $bar->setMessage($this->formatElapsedTime($elapsedTime), 'elapsed');
             }
+
+            $newKeywordsCount = 0;
+            $updatedKeywordsCount = 0;
+
+            DB::transaction(function () use ($keywordData &$newKeywordsCount, &$updatedKeywordsCount) {
+                $result = DB::table('keywords')->upsert($keywordData, 
+                    ['name'], 
+                    [
+                        'template',
+                        'map',
+                        'house_types',
+                        'status_types',
+                        'shortcode',
+                        'listing_settings',
+                        'sponsor_only',
+                        'show_solds',
+                        'use_version_5',
+                        'active',
+                        'listhub',
+                        'created_at',
+                        'updated_at',
+                        'customer_id'
+                    ]
+                );
+                $newKeywordsCount += $result['new'];
+                $updatedKeywordsCount += $result['updated'];
+            });
+
+            $currentTime = Carbon::now();
+            $elapsedTime = $currentTime->diffInSeconds($startTime);
+            $averageTimePerChunk = $elapsedTime / $chunksCount;
+            $remainingChunks = max(0, ceil(($totalKeywords - ($chunkSize * $chunksCount)) / $chunkSize));
+            $estimatedTimeInSeconds = $averageTimePerChunk * $remainingChunks;
+            $estimatedTime = Carbon::now()->addSeconds($estimatedTimeInSeconds);
+            $bar->setMessage($estimatedTime->diffForHumans(null, true, false), 'estimated');
+            $bar->setMessage($this->formatElapsedTime($elapsedTime), 'elapsed');
         });
 
         $bar->finish();
+        
         $this->info("\nKeywords import completed!");
+        $this->comment("New Keywords: {$newKeywordsCount}");
+        $this->comment("Updated Keywords: {$updatedKeywordsCount}");
+
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
+     
+
+     
 
     protected function formatElapsedTime($seconds)
     {
